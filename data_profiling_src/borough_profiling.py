585266ccecba32b6267de5f332884026383ebcf1
Minghao Shao <zssmh98@gmail.com>
2021-12-05 15:52:45 -0500

    Update borough_profiling.py

--- data_profiling_src/borough_profiling.py
+++ data_profiling_src/borough_profiling.py
@@ -1,6 +1,6 @@
 import sys
 from pyspark.sql import SparkSession
-from pyspark.sql.functions import format_string, date_format
+from pyspark.sql.functions import format_string, upper
 
 COLUMN = 'Borough Location'
 
@@ -11,7 +11,7 @@ spark = SparkSession.builder.enableHiveSupport().getOrCreate()
 
 ds_full = spark.read.format('csv').options(header='true', inferschema='true').load(FILE_NAME)
 
-res = ds_full.select(ds_full[COLUMN]).groupby(COLUMN).count().withColumnRenamed("count", "number_count").withColumnRenamed(COLUMN, 'borough').distinct()
+res = ds_full.select(ds_full[COLUMN]).groupby(COLUMN).count().withColumnRenamed("count", "number_count").withColumnRenamed(COLUMN, 'borough').withColumn('borough', upper(col='borough')).distinct()
 res.show()
 #res.select(format_string("%s\t%d", res.borough, res.number_count)).write.save("borough_pro.out", format="text")
 
