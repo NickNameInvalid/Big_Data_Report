51cd989b1428315d6724b41fa4082d08cc8b0b65
Minghao Shao <zssmh98@gmail.com>
2021-12-05 20:38:21 -0500

    update borough

--- data_clean_src/borough_clean.py
+++ data_clean_src/borough_clean.py
@@ -12,7 +12,9 @@ spark = SparkSession.builder.enableHiveSupport().getOrCreate()
 ds_full = spark.read.format('csv').options(header='true', inferschema='true').load(FILE_NAME)
 
 res = ds_full.select("*").withColumn(COLUMN, upper(col=COLUMN)).fillna(subset=COLUMN, value='OTHER')
-res.show()
+# a = res.filter(res[COLUMN] == 'BRONX')
+# a.show()
+# res.show()
 #res.select(format_string("%s\t%d", res.borough, res.number_count)).write.save("borough_pro.out", format="text")
 
 spark.stop()
