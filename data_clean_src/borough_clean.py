782b3c51e9d576488aaca28f379bc3ebb5b158b3
Minghao Shao <zssmh98@gmail.com>
2021-12-05 20:02:48 -0500

    Update borough_clean.py

---
+++ data_clean_src/borough_clean.py
@@ -0,0 +1,18 @@
+import sys
+from pyspark.sql import SparkSession
+from pyspark.sql.functions import format_string, upper
+
+COLUMN = 'Borough Location'
+
+FILE_NAME = '../project_data/data-cityofnewyork-us.xeg4-ic28.csv'
+#FILE_NAME = '/user/CS-GY-6513/project_data/data-cityofnewyork-us.' + sys.argv[1] + '.csv'
+
+spark = SparkSession.builder.enableHiveSupport().getOrCreate()
+
+ds_full = spark.read.format('csv').options(header='true', inferschema='true').load(FILE_NAME)
+
+res = ds_full.select("*").withColumn(COLUMN, upper(col=COLUMN)).fillna(subset=COLUMN, value='OTHER')
+res.show()
+#res.select(format_string("%s\t%d", res.borough, res.number_count)).write.save("borough_pro.out", format="text")
+
+spark.stop()
